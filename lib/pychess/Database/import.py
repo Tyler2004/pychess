e3df6a33ff13fe133e9db51cbdbbcd8f6ec64ea6
gbtami <gbtami@localhost>
2008-11-08 12:11:59 +0000

    speed up with using transaction instead of autocommit

--- lib/pychess/Database/import.py
+++ lib/pychess/Database/import.py
@@ -7,7 +7,7 @@ __builtin__.__dict__['_'] = lambda s: s
 
 from profilehooks import profile
 
-from sqlalchemy import Table, Column, Integer, String, Date, create_engine, ForeignKey, MetaData, select
+from sqlalchemy import Table, Column, Integer, String, Date, create_engine, ForeignKey, MetaData, select, and_
 
 from pychess.Utils.const import *
 from pychess.Savers.pgn import load
@@ -16,12 +16,12 @@ from pychess.System.prefix import addDataPrefix
 
 metadata = MetaData()
 
-name = Table('names', metadata,
+names_table = Table('names', metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String)
     )
 
-game = Table('games', metadata,
+games_table = Table('games', metadata,
     Column('id', Integer, primary_key=True),
     Column('event', Integer, ForeignKey('names.id')),
     Column('site', Integer, ForeignKey('names.id')),
@@ -41,87 +41,100 @@ game = Table('games', metadata,
     Column('movetext', String)
     )
 
-
 #@profile
-def import_from_pgn(file, conn, game_table, name_table):
+def import_from_pgn(file, conn):
     cf = load(open(file))
-    
-    s = select([name_table])
+
+    s = select([names_table])
     names = dict([(n.name, n.id) for n in conn.execute(s)])
 
-    def get_id(name_str):
-        if not name_str:
+    ins_names = names_table.insert()
+    ins_games = games_table.insert()
+
+    def get_id(name):
+        if not name:
             return None
 
-        if name_str in names:
-            return names[name_str]
+        if name in names:
+            return names[name]
         else:
-            ins_name = name_table.insert()
-            result = conn.execute(ins_name, name=name_str)
+            result = conn.execute(ins_names, name=name)
             id = result.last_inserted_ids()[0]
-            names[name_str] = id
+            names[name] = id
             return id
-    
-    for i, game in enumerate(cf.games):
-        event = get_id(cf.get_event(i))
-        site = get_id(cf.get_site(i))
-        y, m, d = cf.get_date(i)
-        game_date = date(y, m, d)
-        round = cf.get_round(i)
-        white, black = cf.get_player_names(i)
-        white = get_id(white)
-        black = get_id(black)
-        result = reprResult[cf.get_result(i)]
-        white_elo, black_elo = cf.get_elo(i)
 
-        ply_count = cf._getTag(i, "PlyCount")
-        y, m, d = cf.get_event_date(i)
-        event_date = date(y, m, d)
-        eco = cf._getTag(i, "ECO")
-        fen = cf._getTag(i, "FEN")
-        annotator = get_id(cf._getTag(i, "Annotator"))
-        source = get_id(cf._getTag(i, "Source"))
-        movetext = game[1]
+    trans = conn.begin()
+    try:
+        for i, game in enumerate(cf.games):
+            event = get_id(cf.get_event(i))
+            site = get_id(cf.get_site(i))
+            y, m, d = cf.get_date(i)
+            game_date = date(y, m, d)
+            round = cf.get_round(i)
+            white, black = cf.get_player_names(i)
+            white = get_id(white)
+            black = get_id(black)
+            result = reprResult[cf.get_result(i)]
+            white_elo, black_elo = cf.get_elo(i)
 
-#        print i, event, site, game_date, round, white, black, result
-        ins_game = game_table.insert()
-        result = conn.execute(ins_game,
-                                event=event,
-                                site=site,
-                                game_date=game_date,
-                                round=round,
-                                white=white,
-                                black=black,
-                                result=result,
-                                white_elo=white_elo,
-                                black_elo=black_elo,
-                                ply_count=ply_count,
-                                event_date=event_date,
-                                eco=eco,
-                                fen=fen,
-                                annotator=annotator,
-                                source=source,
-                                movetext=movetext)
+            ply_count = cf._getTag(i, "PlyCount")
+            y, m, d = cf.get_event_date(i)
+            event_date = date(y, m, d)
+            eco = cf._getTag(i, "ECO")
+            fen = cf._getTag(i, "FEN")
+            annotator = get_id(cf._getTag(i, "Annotator"))
+            source = get_id(cf._getTag(i, "Source"))
+            movetext = game[1]
 
+            print i
+            conn.execute(ins_games,
+                        event=event,
+                        site=site,
+                        game_date=game_date,
+                        round=round,
+                        white=white,
+                        black=black,
+                        result=result,
+                        white_elo=white_elo,
+                        black_elo=black_elo,
+                        ply_count=ply_count,
+                        event_date=event_date,
+                        eco=eco,
+                        fen=fen,
+                        annotator=annotator,
+                        source=source,
+                        movestr=movetext
+                        )
+        trans.commit()
+    except:
+        trans.rollback()
+        raise
 
 if __name__ == "__main__":
     path = "sqlite:///" + os.path.join(addDataPrefix("pychess.db"))
     
-    engine = create_engine(path, echo=True)
-    
-    metadata.drop_all(engine)     
-    metadata.create_all(engine)     
-
+    engine = create_engine(path, echo=False)
     conn = engine.connect()
 
-    import_from_pgn(sys.argv[1], conn, game, name)
+    metadata.drop_all(engine)
+    metadata.create_all(engine)
+
+    import_from_pgn(sys.argv[1], conn)
     
-    s = select([name])
-    names = dict([(n.id, n.name) for n in conn.execute(s)])
-    print names
-    
-    s = select([game])
+    a1 = names_table.alias()
+    a2 = names_table.alias()
+    a3 = names_table.alias()
+    a4 = names_table.alias()
+
+    s = select([a1.c.name, a2.c.name, a3.c.name, a4.c.name,
+                games_table.c.game_date, games_table.c.result],
+                and_(
+                games_table.c.event==a1.c.id,
+                games_table.c.site==a2.c.id,
+                games_table.c.white==a3.c.id,
+                games_table.c.black==a4.c.id))
+                 
     for g in conn.execute(s):
-        print g[0:7]
-    
+        print "%s %s %s %s %s %s" % (g[0], g[1], g[2], g[3], g[4], g[5])
+
     sys.exit()
